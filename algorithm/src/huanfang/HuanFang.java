4d5192d073943cdea23fd4778dd077e978d6edec
谢晶亮 <2461657291@qq.com>
2020-10-13 21:21:57 +0800

    幻方求解

--- algorithm/src/huanfang/HuanFang.java
+++ algorithm/src/huanfang/HuanFang.java
@@ -14,19 +14,18 @@ public class HuanFang {
         int r = 0;//从第一行开始
         int c = n / 2;//从第一行中间的列开始填
         int num = 1;//设置第一位数
-        int array[][] = new int[n][n];//创建n*n的数组
+        huanfang = new int[n][n];//创建n*n的数组
         for(int i = 1; i <= n*n; i++){
-            array[r][c] = num;
+            huanfang[r][c] = num;
             r = (r - 1 + n) % n;
             c = (c + 1 ) % n;//往右上角填幻方
-            if(array[r][c] != 0){
+            if(huanfang[r][c] != 0){
                 //遇到不为空的方格向下平移两行，再向左一列开始填
                 r = (r + 2) % n;
                 c = (c - 1 + n) % n;
             }
             num++;
         }
-        huanfang = array;
     }
     public void get(){
         //获取幻方阶数，并打印出来
