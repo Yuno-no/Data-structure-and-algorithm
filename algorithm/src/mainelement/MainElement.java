d77b15b2a9b2cdbbda423c663eac277e1a386bb7
谢晶亮 <2461657291@qq.com>
2020-10-14 20:19:24 +0800

    求解主元算法

---
+++ algorithm/src/mainelement/MainElement.java
@@ -0,0 +1,32 @@
+package mainelement;
+
+/**
+ * @author Yuno
+ * @create 2020/10/14-18:55
+ */
+public class MainElement {
+    int[] nums;//设置变量存放输入的数组
+
+    public MainElement(int[] nums){
+        this.nums = nums;
+        int len = nums.length;
+        for (int i = 0; i < len; i++){
+            nums[nums[i]%len] = nums[nums[i]%len] + len;
+        }
+    }
+    public void printME(){
+        for (int i = 0; i < nums.length; i++){
+            if ((nums[i] / nums.length) > (nums.length/ 2)){
+                System.out.println("该数组的主元为" + i);
+                return;
+            }
+        }
+        System.out.println("无主元");
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {8, 6, 8, 7, 1, 8, 8, 7, 8, 6, 8, 4, 4, 8, 8, 8, 2};
+        MainElement mainelement = new MainElement(nums);
+        mainelement.printME();
+    }
+}
