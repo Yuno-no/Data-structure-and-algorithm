fb1b090436bd37db0acaf1cd02a36604cb3d36e1
谢晶亮 <2461657291@qq.com>
2020-10-18 19:24:21 +0800

    求解最小正整数

--- algorithm/src/firstMissingPositive/FirstMissingPositive.java
+++ algorithm/src/firstMissingPositive/FirstMissingPositive.java
@@ -1,5 +1,7 @@
 package firstMissingPositive;
 
+import java.util.Scanner;
+
 /**
  * @author Yuno
  * @create 2020/10/18-17:15
@@ -27,12 +29,16 @@ public class FirstMissingPositive {
     public int calculate(){
         int len = nums.length;
         for (int i = 0; i < len; i++){
+            //遍历数组，当元素为[1，N]时，将nums[i]与nums[nums[i] - 1]置换
+            //且元素不大于数组长度，且nums[i]与nums[nums[i] - 1]不相等
             if (nums[i] > 0 && nums[i] <= len && nums[nums[i] - 1] != nums[i]){
                 int temp = nums[nums[i] - 1];
                 nums[nums[i] - 1] = nums[i];
                 nums[i] = temp;
             }
         }
+
+        //遍历经过置换后的数组，判断元素是否在正确的位置，如果nums[i] != i + 1，则i + 1 为最小正整数
         for (int i = 0; i < len; i++){
             if (nums[i] != i + 1){
                 return i + 1;
@@ -42,7 +48,15 @@ public class FirstMissingPositive {
     }
 
     public static void main(String[] args) {
-        int[] nums = {-1, 5, 4, 3, 8};
+//      int[] nums = {-1, 5, 4, 3, 8};
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入数组长度");
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        System.out.println("请输入数组的每个元素");
+        for (int i = 0; i < nums.length; i++){
+            nums[i] = sc.nextInt();
+        }
         FirstMissingPositive fmp = new FirstMissingPositive(nums);
         System.out.println(fmp.calculate());
     }
