c26c44af5f668c3ef77a21e8d66b88db10e2a935
谢晶亮 <2461657291@qq.com>
2020-11-26 21:05:22 +0800

    二叉树的二叉链表表示法; 及递归遍历二叉树

--- algorithm/src/binary_tree/BtTest.java
+++ algorithm/src/binary_tree/BtTest.java
@@ -14,7 +14,11 @@ public class BtTest {
         strtree.inorder();
         System.out.println("后根遍历二叉树：");
         strtree.postorder();
+        strtree.insert(strtree.root.left, true,"X");//插入x结点作为B结点的左孩子
+        System.out.println("插入x后先根表示的二叉树为：");
+        strtree.preorder();
+        strtree.insert("Z");//插入z作为根节点
+        System.out.println("z作为跟结点后的二叉树的先根表示");
+        strtree.preorder();
     }
-
-
 }
