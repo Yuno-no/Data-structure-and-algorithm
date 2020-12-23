610b1e039de263f64fa0ff6d1f59aeac54e82148
谢晶亮 <2461657291@qq.com>
2020-12-23 21:30:40 +0800

    迪克斯特拉算法求最小路径

--- algorithm/src/Dijkstra/shortestPath.java
+++ algorithm/src/Dijkstra/shortestPath.java
@@ -9,13 +9,16 @@ public class shortestPath {
      * 参数adjMatrix:为图的权重矩阵，权值为-1的两个顶点表示不能直接相连
      * 函数功能：返回顶点0到其它所有顶点的最短距离，其中顶点0到顶点0的最短距离为0
      */
+    int[] path;  //用于存放最短路径上次经过的定点
     public int[] getShortestPaths(int[][] adjMatrix) {
         int[] result = new int[adjMatrix.length];   //用于存放顶点0到其它顶点的最短距离
         boolean[] used = new boolean[adjMatrix.length];  //用于判断顶点是否被遍历
+        path = new int[adjMatrix.length];  //用于存放最短路径上次经过的定点
         used[0] = true;  //表示顶点0已被遍历
         for(int i = 1;i < adjMatrix.length;i++) {
             result[i] = adjMatrix[0][i];
             used[i] = false;
+            path[i] = (adjMatrix[0][i] != -1) ? 0 : -1;
         }
 
         for(int i = 1;i < adjMatrix.length;i++) {
@@ -28,11 +31,15 @@ public class shortestPath {
                 }
             }
             used[k] = true;    //将距离最小的顶点，记为已遍历
-            for(int j = 1;j < adjMatrix.length;j++) {  //然后，将顶点0到其它顶点的距离与加入中间顶点k之后的距离进行比较，更新最短距离
+            for(int j = 1;j < adjMatrix.length;j++) {
+                //然后，将顶点0到其它顶点的距离与加入中间顶点k之后的距离进行比较，更新最短距离
                 if(!used[j]) {  //当顶点j未被遍历时
-                    //首先，顶点k到顶点j要能通行；这时，当顶点0到顶点j的距离大于顶点0到k再到j的距离或者顶点0无法直接到达顶点j时，更新顶点0到顶点j的最短距离
-                    if(adjMatrix[k][j] != -1 && (result[j] > min + adjMatrix[k][j] || result[j] == -1))
+                    //首先，顶点k到顶点j要能通行；
+                    // 这时，当顶点0到顶点j的距离大于顶点0到k再到j的距离或者顶点0无法直接到达顶点j时，更新顶点0到顶点j的最短距离
+                    if(adjMatrix[k][j] != -1 && (result[j] > min + adjMatrix[k][j] || result[j] == -1)){
                         result[j] = min + adjMatrix[k][j];
+                        path[j] = k;
+                    }
                 }
             }
         }
@@ -48,8 +55,14 @@ public class shortestPath {
                 {-1,-1,4,2,0,5},
                 {-1,-1,-1,3,5,0}};
         int[] result = test.getShortestPaths(adjMatrix);
+        int[] path = test.path;
         System.out.println("顶点0到图中所有顶点之间的最短距离为：");
-        for(int i = 0;i < result.length;i++)
+        for(int i = 0; i < result.length; i++)
             System.out.print(result[i]+" ");
+        System.out.println();
+        System.out.println("顶点0到图中所有顶点最短距离上一次经过的顶点为：");
+        for (int i = 0; i < path.length; i++){
+            System.out.print(path[i] + " ");
+        }
     }
 }
